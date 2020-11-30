13762e8658fa72b567270176538b0c8588d6875a
David French <David@mefalf.net>
2020-11-29 19:09:29 -0600

    adding the app to the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,20 @@ const path = require("path");
 
 module.exports = function (app, fs) {
 
+    const db = require("./db/db.json");
+
+
+    app.get("/api/notes/", function (req, res) {
+        note_data.push(req.body);
+        res.json(true);
+    });
+
+    app.get("/api/notes/", function (req, res) {
+        res.json(note_data);
+    });
+
+    app.get("/api/notes/", function(req, res){
+        note_data.length = 0;
+        res.json({ok: true});
+    });
 }
--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const path = require("path")
 const fs = require("fs")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({exteneded: ture}));
+app.use(express.json());
+app.use(express.static("./public/"));
+
+require("./routes/apiRoutes")(app, fs);
+require("./routes/htmlRoutes")(app, fs);
+
+app.listen(PORT, function() {
+    console.log("listening on LocalHost: " + PORT);
+})
