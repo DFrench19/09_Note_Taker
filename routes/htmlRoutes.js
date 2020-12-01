0f8d3fa39c159d406521e6a80fff771533cfecfa
David French <David@mefalf.net>
2020-12-01 14:03:09 -0600

    updating htmlRoutes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,4 +2,14 @@ const path = require("path");
 
 module.exports = function (app, fs) {
 
+
+    app.get('/notes', function (res, req) {
+        res.sendFile(path.join(_dirname, '../public/notes.html'))
+    })
+
+    app.get('/', function (res, req) {
+        res.sendFile(path.join(_dirname, '../public/index.html'))
+    })
+
+
 }
